7b896997414b53accc36d13f5dd0433fdd39989a
evanjkish <evanjkish@icloud.com>
2021-11-29 17:50:19 -0700

    Wrote code to log 3 lines about myself..

---
+++ First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) { 
+        System.out.println("My name is Evan.");
+        System.out.println("I am 25 years old.");
+        System.out.println("My hometown is North Richland Hills, TX.");
+    }
+}
